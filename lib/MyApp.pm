daf57916dd7da0b9a863c8e9366bb1c57a9bfb42
t0m <bobtfish@bobtfish.net>
2009-04-24 23:59:48 +0100

    Rename crap

--- lib/MyApp.pm
+++ lib/MyApp.pm
@@ -2,63 +2,39 @@ package MyApp;
 
 use strict;
 use warnings;
+use Catalyst qw<
+                   -Debug
+               >;
 
-use Catalyst::Runtime '5.70';
+__PACKAGE__->config(
+    name                  => 'MyApp',
+);
 
-# Set flags and add plugins for the application
-#
-#         -Debug: activates the debug mode for very useful log messages
-#   ConfigLoader: will load the configuration from a Config::General file in the
-#                 application's home directory
-# Static::Simple: will serve static files from the application's root
-#                 directory
-
-use parent qw/Catalyst/;
-use Catalyst qw/-Debug
-                ConfigLoader
-                Static::Simple/;
-our $VERSION = '0.01';
-
-# Configure the application.
-#
-# Note that settings in myapp.conf (or other external
-# configuration file that you set up manually) take precedence
-# over this when using ConfigLoader. Thus configuration
-# details given here can function as a default configuration,
-# with an external configuration file acting as an override for
-# local deployment.
-
-__PACKAGE__->config( name => 'MyApp' );
-
-# Start the application
-__PACKAGE__->setup();
+__PACKAGE__->setup;
 
 
-=head1 NAME
+sub chain :Chained('/') PathPart('foo') CaptureArgs(1){
+    my( $self, $c, $arg ) = @_;
+    $c->stash->{chain_called} ||= 0;
+    $c->stash->{chain_called}++;
+    $c->log->debug('chain action happens: ', $arg);
+}
 
-MyApp - Catalyst based application
+sub end_of_chain : Chained('chain') PathPart('bar') Args(0){
+    my( $self, $c ) = @_;
+    $c->log->debug('end of chain action happens!');
+}
 
-=head1 SYNOPSIS
+sub magic_side_request :Local{
+    my( $self, $c, @rest_of_path ) = @_;
+    $c->debug('magic side request happens!');
 
-    script/myapp_server.pl
+    my $path = '/'.join '/', @rest_of_path;
 
-=head1 DESCRIPTION
+    $c->request->path($path);
+    $c->dispatcher->prepare_action($c);
 
-[enter your description here]
+    $c->go($c->action, $c->request->args,  $c->request->captures);
+}
 
-=head1 SEE ALSO
-
-L<MyApp::Controller::Root>, L<Catalyst>
-
-=head1 AUTHOR
-
-Tomas Doran
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
-
-1;
+;1;
